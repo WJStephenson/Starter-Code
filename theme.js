2f0e595175a18f2cb2edc2c840cd19e3afe70422
WJStephenson <willjosteph@gmail.com>
2023-07-10 20:57:26 +0100

    added sessionStorage memory for theme

--- theme.js
+++ theme.js
@@ -1,13 +1,11 @@
 const button = document.getElementById('theme-button');
 const lightModeIcon = `<i class="fa-regular fa-sun"></i>`;
 const darkModeIcon = `<i class="fa-regular fa-moon"></i>`;
-sessionStorage.setItem('mode', 'light');
-
-const lightColors = [`#ff8a00`,
+const darkColors = [`#ff8a00`,
     `#fff`,
     `#13071c`,
     `#1a0725`];
-const darkColors = [`#ff8a00`,
+const lightColors = [`#ff8a00`,
     `#fff`,
     `#fff`,
     `#E6E6E6`];
@@ -16,21 +14,45 @@ button.addEventListener('click', () => {
     setTheme();
 });
 
+window.addEventListener('load', () => {  
+    let mode = sessionStorage.getItem('mode');
+    if (mode === null) {
+        mode = 'dark';
+        setDarkMode();
+    } else if (mode === 'dark') {
+        setDarkMode();
+    } else {
+        setLightMode();
+    }
+});
+
 function setTheme() {
     let mode = sessionStorage.getItem('mode');
-    if (mode === 'light') {
-        sessionStorage.setItem('mode', 'dark');
-        document.documentElement.style.setProperty('--primary-text', darkColors[0]);
-        document.documentElement.style.setProperty('--secondary-text', darkColors[1]);
-        document.documentElement.style.setProperty('--primary-bg', darkColors[2]);
-        document.documentElement.style.setProperty('--secondary-bg', darkColors[3]);
-        button.innerHTML = darkModeIcon;
+    if (mode === 'dark') {
+        console.log('currently dark, switching to light');
+        setLightMode();
     } else {
-        sessionStorage.setItem('mode', 'light');
-        document.documentElement.style.setProperty('--primary-text', lightColors[0]);
-        document.documentElement.style.setProperty('--secondary-text', lightColors[1]);
-        document.documentElement.style.setProperty('--primary-bg', lightColors[2]);
-        document.documentElement.style.setProperty('--secondary-bg', lightColors[3]);
-        button.innerHTML = lightModeIcon;
+        console.log('currently light, switching to dark');
+        setDarkMode();
     }
+}
+
+function setDarkMode() {
+    console.log('setting dark');
+    sessionStorage.setItem('mode', 'dark');
+    document.documentElement.style.setProperty('--primary-text', darkColors[0]);
+    document.documentElement.style.setProperty('--secondary-text', darkColors[1]);
+    document.documentElement.style.setProperty('--primary-bg', darkColors[2]);
+    document.documentElement.style.setProperty('--secondary-bg', darkColors[3]);
+    button.innerHTML = darkModeIcon;
+}
+
+function setLightMode() {
+    console.log('setting light');
+    sessionStorage.setItem('mode', 'light');
+    document.documentElement.style.setProperty('--primary-text', lightColors[0]);
+    document.documentElement.style.setProperty('--secondary-text', lightColors[1]);
+    document.documentElement.style.setProperty('--primary-bg', lightColors[2]);
+    document.documentElement.style.setProperty('--secondary-bg', lightColors[3]);
+    button.innerHTML = lightModeIcon;
 }
